7f211d3b2d148e657f5aab7561549e566dc7169a
Sebastian Toulier <sebaas14toulier@gmail.com>
2022-10-03 19:39:29 -0500

    feat: refactor env

--- src/views/historic/components/historicController.ts
+++ src/views/historic/components/historicController.ts
@@ -1,11 +1,10 @@
 import {ICardTimeSeries, ISeries} from "@/components/highchart/splineGraph/splineController";
 import axios from "axios";
 import {reactive} from "vue";
-import {SeriesSplineOptions} from "highcharts";
 
 export class HistoricController {
-    private _temperatureData: ISeries  | any| undefined = undefined;
-    private _temperatureSeries: ICardTimeSeries ;
+    private _temperatureData: ISeries | any | undefined = undefined;
+    private _temperatureSeries: ICardTimeSeries;
 
     constructor() {
         this._temperatureSeries = {
@@ -16,10 +15,8 @@ export class HistoricController {
         }
     }
 
-
-
-    async loadData(){
-        this._temperatureData = (await axios.get('http://20.206.200.27:1880/data/historico/humedad/date/1664057282')).data;
+    async loadData() {
+        this._temperatureData = (await axios.get(process.env.VUE_APP_BASE_URL_API + '/data/historico/humedad/date/1664057282')).data;
         console.log(this._temperatureData);
         this._temperatureSeries.series.at(0).data = this._temperatureData.data;
         //TODO: REMOVE THIS
